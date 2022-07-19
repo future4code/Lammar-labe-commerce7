c516ff3ee4450413ed43a8f60d922a51b52bacb3
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-07-18 21:41:49 -0300

    Criação da estutura estática do card de produto

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -2,15 +2,50 @@ import React from 'react';
 import styled from "styled-components"
 
 const ContainerProdutos = styled.div `
-    height: 100em;
-    padding: 1em;
+    display:flex;
+    flex-direction: column;
+    height: 20em;
+    width: 15em;
+    border: 3px solid white;
+    border-radius: 10px;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    margin: 1em;
+    padding-bottom: 1em;
+    padding-top: 1em;
+
+    button {
+        border-radius: 5px;
+        border: 2px solid white;
+        width: 10vw;
+        height: 4vh;
+    }
+
+    button:hover {
+        background-color: black;
+        border: 2px solid black;
+        color: white;
+    }
+
+    img {
+        height: 15em;
+        border-radius: 10px;
+        border: 2px solid white;
+
+    }
 `
-function Produtos () {
+function Produtos (props) {
   
     return (
-        <ContainerProdutos>
-            <h1>Produtos</h1>
-        </ContainerProdutos>
+        <div>
+            <ContainerProdutos>
+                <img alt="imagem do traje espacial" src="https://ids.si.edu/ids/deliveryService?id=NASM-SI-2003-27341&max=900"/>
+                <p> nome do produto </p>
+                <p> R$ </p>
+                <button>Adicionar ao carrinho</button>
+            </ContainerProdutos>
+        </div>
     )
 }
 
